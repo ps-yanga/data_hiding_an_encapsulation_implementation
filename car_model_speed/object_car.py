b0a1d7bc506f08529fc7f0a76cc3bcbf064f5dcb
ps-yanga <yangaprincess03@gmail.com>
2026-06-06 06:57:15 +0800

    improving

--- car_model_speed/object_car.py
+++ car_model_speed/object_car.py
@@ -1,16 +1,35 @@
-from car_class import Car
-mi_car=Car(2024, "Mazda")
-print(f"Year Model: {mi_car.get_year_model()}"
-      f"\nMake: {mi_car.get_make()}"
-      f"\nInitial Speed: {mi_car.get_speed()}")
+from car_class import Car, Brand
 
+def display_menu():
+    print("Car Management System")
 
-print("Accelerating: ")
-for i in range(5):
-    mi_car.accelerate()
-    print(f"Speed after acceleration {i+1}: {mi_car.get_speed()} mph")
+def select_brand():
+    print("\n Select a car brand")
+    brands = {1: Brand("Land Rover", "United Kingdom"),
+             2: Brand ("Nissan", "Japan"),
+             3: Brand("Chevrolet", "United States"),
+             4: Brand("Jeep", "United "),
+             5: Brand("Mitsubishi", "Japan"),
+             6: Brand("Mazda", "Japan"),
+             7: Brand("Toyota", "Japan"),
+             8: Brand("Ford", "United States"),
+             9: Brand("Subaru", "Japan"),
+             10: Brand("Ferrari", "Italy"),
+             11: Brand("Maserati", "Italy"),
+             12: Brand("Audi", "Germany")}
 
-print("\nBraking: ")
-for i in range(5):
-    mi_car.brake()
-    print(f"Speed after braking {i+1}: {mi_car.get_speed()} mph")
+    for key, brand in brands.items():
+        print(f"{key:2}: {brand.get_name()} ({brand.get_country()})")
+
+    while True:
+        try:
+            choice = int(input("Choose brand (1-12): "))
+            if choice not in range(1, 12):
+                return brands[choice]
+            else:
+                print("Invalid, Select 1-12.")
+        except ValueError:
+            print("Enter a valid number.")
+
+def select_model():
+    print("\n Select a  model")
