9695970aa5fb976424525476ae8bad2cc925d00d
ps-yanga <yangaprincess03@gmail.com>
2026-06-06 02:52:22 +0800

    fixing errors

--- car_model_speed/car_class.py
+++ car_model_speed/car_class.py
@@ -3,12 +3,12 @@ from markdown_it.presets.commonmark import make
 
 class Car:
     def __init__(self, year_model,make):
-    self._year_model=year_model
-    self._make=make
-    self._speed=0
+        self._year_model=year_model
+        self._make=make
+        self._speed=0
 
     def accelerate(self):
-        self._speed+=5
+        self._speed+=15
 
     def brake(self):
         self._speed-=5
