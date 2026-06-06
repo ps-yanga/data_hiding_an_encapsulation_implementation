f39e81ba4a11b85535f9969a7dd2cf932827713d
ps-yanga <yangaprincess03@gmail.com>
2026-06-06 09:08:05 +0800

    setters

---
+++ pet_name/pet_class.py
@@ -0,0 +1,16 @@
+class Pet:
+    def __int__(self):
+        self._name=""
+        self._animal_type=""
+        self._age=0
+
+    def set_name(self, name):
+        self._name = name
+
+    def set_animal_type(self, animal_type):
+        self._animal_type = animal_type
+
+    def set_age(self, age):
+        self._age = age
+
+    
